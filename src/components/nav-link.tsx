a5d873cdae872b1d6c35fe0c16c267feb6b1e7a8
Adeonir Kohl <adeonir@gmail.com>
2024-02-23 12:16:45 -0300

    Create nav-link component

---
+++ src/components/nav-link.tsx
@@ -0,0 +1,19 @@
+import type { LinkProps } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+
+import { cn } from '~/lib/utils'
+
+export const NavLink = (props: LinkProps) => {
+  const { pathname } = useLocation()
+
+  return (
+    <Link
+      className={cn(
+        'flex items-center gap-2 text-muted-foreground transition hover:text-primary',
+        'data-[active=true]:font-bold data-[active=true]:text-primary',
+      )}
+      data-active={pathname === props.to}
+      {...props}
+    />
+  )
+}
