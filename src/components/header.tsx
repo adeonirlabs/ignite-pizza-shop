e6c8d36954dbe54f314ccbe31277cb58702f2ce9
Adeonir Kohl <adeonir@gmail.com>
2024-02-23 12:17:02 -0300

    Create header component

---
+++ src/components/header.tsx
@@ -0,0 +1,40 @@
+import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
+import type { ComponentProps } from 'react'
+
+import { NavLink } from '~/components/nav-link'
+import { Separator } from '~/components/ui/separator'
+import { cn } from '~/lib/utils'
+
+const links = [
+  {
+    href: '/',
+    label: 'Início',
+    icon: <Home className="size-5" />,
+  },
+  {
+    href: '/orders',
+    label: 'Pedidos',
+    icon: <UtensilsCrossed className="size-5" />,
+  },
+]
+
+const Header = ({ className, ...props }: ComponentProps<'header'>) => {
+  return (
+    <header className={cn('border-b', className)} {...props}>
+      <div className="flex h-16 items-center gap-6 px-10">
+        <Pizza className="size-6" />
+        <Separator className="h-8" orientation="vertical" />
+        <nav className="flex items-center gap-6">
+          {links.map(({ href, label, icon }) => (
+            <NavLink key={href} to={href}>
+              {icon}
+              {label}
+            </NavLink>
+          ))}
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export { Header }
