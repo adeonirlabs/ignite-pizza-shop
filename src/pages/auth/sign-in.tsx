22d41de62554d217a88014d6ca1d72d0191c1f6b
Adeonir Kohl <adeonir@gmail.com>
2024-02-23 08:52:11 -0300

    Update sign-in page with content and form

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,29 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Button } from '~/components/ui/button'
+import { Input } from '~/components/ui/input'
+import { Label } from '~/components/ui/label'
+
 export function SignIn() {
   return (
     <>
       <Helmet title="Sign In" />
-      <h1>Sign In</h1>
+      <section className="flex w-full max-w-80 flex-col justify-center gap-6">
+        <header className="space-y-2 text-center">
+          <h1 className="text-2xl font-semibold">Acessar o painel</h1>
+          <p className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro!</p>
+        </header>
+        <form className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">E-mail</Label>
+            <Input id="email" placeholder="Seu e-mail" type="email" />
+          </div>
+
+          <Button className="w-full" type="submit">
+            Entrar
+          </Button>
+        </form>
+      </section>
     </>
   )
 }
