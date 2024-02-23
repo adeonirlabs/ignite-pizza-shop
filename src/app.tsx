8abd87508dc21e0def13a4f895140802487ab246
Adeonir Kohl <adeonir@gmail.com>
2024-02-23 09:29:19 -0300

    Install and setup sonnar toast

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { Toaster } from './components/ui/sonner'
 import { router } from './routes'
 
 export function App() {
@@ -10,6 +11,7 @@ export function App() {
     <HelmetProvider>
       <Helmet titleTemplate="%s | Pizza Shop" />
       <RouterProvider router={router} />
+      <Toaster closeButton richColors />
     </HelmetProvider>
   )
 }
