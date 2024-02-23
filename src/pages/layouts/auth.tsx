851b92afdf2f3f63050f43f4dfbcbdbd0447ae06
Adeonir Kohl <adeonir@gmail.com>
2024-02-23 08:07:54 -0300

    Add base content and styles to auth layout

--- src/pages/layouts/auth.tsx
+++ src/pages/layouts/auth.tsx
@@ -1,12 +1,21 @@
+import { Pizza } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <>
-      <h1>Auth Layout</h1>
-      <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-r-2 border-foreground/5 bg-muted p-10">
+        <header className="flex items-center gap-3 text-foreground">
+          <Pizza className="size-5" />
+          <span className="text-xl font-semibold">Pizza Shop</span>
+        </header>
+        <footer className="text-xs text-muted-foreground">
+          Painel do parceiro &copy; Pizza Shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center p-10">
         <Outlet />
       </div>
-    </>
+    </div>
   )
 }
