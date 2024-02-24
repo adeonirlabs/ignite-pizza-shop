84f3a8ed9ecc4daa55e331fae86b875c152d54f4
Adeonir Kohl <adeonir@gmail.com>
2024-02-24 10:05:05 -0300

    Add theme provider to app component

--- src/app.tsx
+++ src/app.tsx
@@ -3,14 +3,18 @@ import './globals.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
-import { Toaster } from './components/ui/sonner'
+import { Toaster } from '~/components/ui/sonner'
+import { ThemeProvider } from '~/providers/theme'
+
 import { router } from './routes'
 
 export const App = () => {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | Pizza Shop" />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="light" storageKey="pizza-shop-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
       <Toaster />
     </HelmetProvider>
   )
