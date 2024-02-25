620cc3bad1049f193aa9ca0f7dd5d0fddef76708
Adeonir Kohl <adeonir@gmail.com>
2024-02-24 21:23:51 -0300

    Add cards to dashboard page

--- src/pages/app/dashboard/index.tsx
+++ src/pages/app/dashboard/index.tsx
@@ -1,5 +1,10 @@
 import { Helmet } from 'react-helmet-async'
 
+import { CancelledByMonthCard } from './components/canceled-by-month-card'
+import { OrdersByDayCard } from './components/orders-by-day-card'
+import { OrdersByMonthCard } from './components/orders-by-month-card'
+import { RevenueCard } from './components/renevue-card'
+
 export const Dashboard = () => {
   return (
     <>
@@ -7,7 +12,12 @@ export const Dashboard = () => {
       <header className="flex items-center justify-between">
         <h1 className="text-3xl font-bold">Dashboard</h1>
       </header>
-      <section className="grid grid-cols-4 gap-4">{/* ... */}</section>
+      <section className="grid grid-cols-4 gap-4">
+        <RevenueCard />
+        <OrdersByMonthCard />
+        <OrdersByDayCard />
+        <CancelledByMonthCard />
+      </section>
     </>
   )
 }
