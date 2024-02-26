3839155f3411a7a76fe1723dc3ec753eee0bb717
Adeonir Kohl <adeonir@gmail.com>
2024-02-26 08:17:16 -0300

    Create sign-in api hook

---
+++ src/api/sign-in/index.tsx
@@ -0,0 +1,26 @@
+import { useMutation } from '@tanstack/react-query'
+
+import { api } from '~/lib/axios'
+
+import type { SignInRequest } from './types'
+
+const endpoints = {
+  signIn: '/authenticate',
+}
+
+const signInKeys = {
+  all: ['signIn'] as const,
+  lists: () => [...signInKeys.all, 'list'] as const,
+  list: (filters: string) => [...signInKeys.lists(), { filters }] as const,
+  details: () => [...signInKeys.all, 'detail'] as const,
+  detail: (id: number) => [...signInKeys.details(), id] as const,
+}
+
+const signInQueries = {
+  useSignInQuery: () =>
+    useMutation({
+      mutationFn: async ({ email }: SignInRequest) => api.post(endpoints.signIn, { email }),
+    }),
+}
+
+export const { useSignInQuery } = signInQueries
