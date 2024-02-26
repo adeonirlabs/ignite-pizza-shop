d5d00cf4d09669ac69104e33192642ca29722b4a
Adeonir Kohl <adeonir@gmail.com>
2024-02-26 09:46:41 -0300

    Add timeout to axios when in dev mode

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,17 @@ import axios from 'axios'
 
 import { env } from '~/env'
 
+import { sleep } from './utils'
+
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true,
 })
+
+if (env.DEV) {
+  api.interceptors.response.use(async (config) => {
+    await sleep(500)
+
+    return config
+  })
+}
