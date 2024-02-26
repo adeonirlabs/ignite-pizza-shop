ed1588fca80edc3d75ae03127ef4c33eb5f46959
Adeonir Kohl <adeonir@gmail.com>
2024-02-26 08:54:12 -0300

    Create sign-up api hook

---
+++ src/api/sign-up/index.tsx
@@ -0,0 +1,26 @@
+import { useMutation } from '@tanstack/react-query'
+
+import { api } from '~/lib/axios'
+
+import type { SignUpRequest } from './types'
+
+const endpoints = {
+  signUp: '/restaurants',
+}
+
+const signUpKeys = {
+  all: ['signUp'] as const,
+  lists: () => [...signUpKeys.all, 'list'] as const,
+  list: (filters: string) => [...signUpKeys.lists(), { filters }] as const,
+  details: () => [...signUpKeys.all, 'detail'] as const,
+  detail: (id: number) => [...signUpKeys.details(), id] as const,
+}
+
+const signUpQueries = {
+  useSignUpMutation: () =>
+    useMutation({
+      mutationFn: async (body: SignUpRequest) => api.post(endpoints.signUp, body),
+    }),
+}
+
+export const { useSignUpMutation } = signUpQueries
---
+++ src/api/sign-up/types.ts
@@ -0,0 +1,6 @@
+export interface SignUpRequest {
+  restaurantName: string
+  managerName: string
+  email: string
+  phone: string
+}
