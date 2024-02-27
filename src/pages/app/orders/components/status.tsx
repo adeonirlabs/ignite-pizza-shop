f2cf640de1b5ce7906515f56e85155a9a735f237
Adeonir Kohl <adeonir@gmail.com>
2024-02-27 08:26:55 -0300

    Update order status to handle label and color dynamicaly

--- src/pages/app/orders/components/status.tsx
+++ src/pages/app/orders/components/status.tsx
@@ -1,8 +1,23 @@
-export const OrderStatus = () => {
+import type { Status } from '~/api/orders/types'
+import { cn } from '~/lib/utils'
+
+interface OrderStatusProps {
+  status: Status
+}
+
+const statusMap: Record<Status, { label: string; color: string }> = {
+  pending: { label: 'Pendente', color: 'bg-gray-400' },
+  canceled: { label: 'Cancelado', color: 'bg-red-400' },
+  processing: { label: 'Em preparo', color: 'bg-yellow-400' },
+  delivering: { label: 'Em entrega', color: 'bg-green-400' },
+  delivered: { label: 'Entregue', color: 'bg-blue-400' },
+}
+
+export const OrderStatus = ({ status }: OrderStatusProps) => {
   return (
     <div className="flex items-center gap-2">
-      <span className="size-2 rounded-full bg-zinc-400" />
-      <span>Pendente</span>
+      <span className={cn('size-2 rounded-full', statusMap[status].color)} />
+      <span>{statusMap[status].label}</span>
     </div>
   )
 }
