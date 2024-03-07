a7858e78c9cc478f21a35e92520c7fcc3a28deeb
Adeonir Kohl <adeonir@gmail.com>
2024-03-07 18:35:01 -0300

    Add order details types

--- src/api/orders/types.ts
+++ src/api/orders/types.ts
@@ -1,3 +1,5 @@
+export type Status = 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+
 export interface Order {
   orderId: string
   status: Status
@@ -6,12 +8,36 @@ export interface Order {
   createdAt: string
 }
 
-export type Status = 'all' | 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
-
 export interface Meta {
   pageIndex: number
   perPage: number
   totalCount: number
+}
+
+export interface Customer {
+  name: string
+  email: string
+  phone: string | null
+}
+
+export interface Product {
+  name: string
+}
+
+export interface OrderItems {
+  id: string
+  priceInCents: number
+  quantity: number
+  product: Product
+}
+
+export interface OrderDetails {
+  id: string
+  createdAt: string
+  status: Status
+  totalInCents: number
+  customer: Customer
+  orderItems: OrderItems[]
 }
 
 export interface OrdersResponse {
@@ -23,5 +49,7 @@ export interface OrdersRequest {
   pageIndex?: number | null
   orderId?: string | null
   customerName?: string | null
-  status?: Status | null
+  status?: Status | 'all' | null
 }
+
+export type OrderDetailsResponse = OrderDetails
