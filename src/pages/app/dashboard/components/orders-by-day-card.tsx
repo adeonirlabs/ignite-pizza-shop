5b029619ecf9633e4ed9c2783616d07051c1787f
Adeonir Kohl <adeonir@gmail.com>
2024-03-08 00:06:37 -0300

    Update orders by day card to fetch data

--- src/pages/app/dashboard/components/orders-by-day-card.tsx
+++ src/pages/app/dashboard/components/orders-by-day-card.tsx
@@ -1,8 +1,15 @@
 import { Utensils } from 'lucide-react'
 
+import { useDayOrdersQuery } from '~/api/metrics'
 import { Card } from '~/components/ui/card'
 
+import { ShowDiff } from './show-diff'
+
 export const OrdersByDayCard = () => {
+  const { data } = useDayOrdersQuery()
+
+  if (!data) return null
+
   return (
     <Card>
       <Card.Header className="flex-row items-center justify-between">
@@ -10,9 +17,9 @@ export const OrdersByDayCard = () => {
         <Utensils className="size-5 text-muted-foreground" />
       </Card.Header>
       <Card.Content className="space-y-2">
-        <span className="block text-3xl font-bold">12</span>
+        <span className="block text-3xl font-bold">{data.amount}</span>
         <span className="block text-xs text-muted-foreground">
-          <span className="text-rose-500 dark:text-rose-400">-4%</span> em relação a ontem
+          <ShowDiff value={data.diffFromYesterday} /> em relação a ontem
         </span>
       </Card.Content>
     </Card>
