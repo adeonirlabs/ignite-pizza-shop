abde9926edeb94c3e814cfaa3b9af4b2d15a3e80
Adeonir Kohl <adeonir@gmail.com>
2024-03-08 00:07:04 -0300

    Update month canceled orders card to fetch data

--- src/pages/app/dashboard/components/canceled-by-month-card.tsx
+++ src/pages/app/dashboard/components/canceled-by-month-card.tsx
@@ -1,8 +1,15 @@
 import { LineChart } from 'lucide-react'
 
+import { useMonthCanceledOrdersQuery } from '~/api/metrics'
 import { Card } from '~/components/ui/card'
 
+import { ShowDiff } from './show-diff'
+
 export const CancelledByMonthCard = () => {
+  const { data } = useMonthCanceledOrdersQuery()
+
+  if (!data) return null
+
   return (
     <Card>
       <Card.Header className="flex-row items-center justify-between">
@@ -10,9 +17,9 @@ export const CancelledByMonthCard = () => {
         <LineChart className="size-5 text-muted-foreground" />
       </Card.Header>
       <Card.Content className="space-y-2">
-        <span className="block text-3xl font-bold">37</span>
+        <span className="block text-3xl font-bold">{data.amount}</span>
         <span className="block text-xs text-muted-foreground">
-          <span className="text-rose-500 dark:text-rose-400">+2%</span> em relação ao mês passado
+          <ShowDiff value={-data.diffFromLastMonth} /> em relação ao mês passado
         </span>
       </Card.Content>
     </Card>
