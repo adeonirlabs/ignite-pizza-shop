2ae5309b0e30ece12587b4c6102afa5018aec21b
Adeonir Kohl <adeonir@gmail.com>
2024-03-08 00:06:28 -0300

    Update orders by month card to fetch data

--- src/pages/app/dashboard/components/orders-by-month-card.tsx
+++ src/pages/app/dashboard/components/orders-by-month-card.tsx
@@ -1,8 +1,15 @@
 import { Utensils } from 'lucide-react'
 
+import { useMonthOrdersQuery } from '~/api/metrics'
 import { Card } from '~/components/ui/card'
 
+import { ShowDiff } from './show-diff'
+
 export const OrdersByMonthCard = () => {
+  const { data } = useMonthOrdersQuery()
+
+  if (!data) return null
+
   return (
     <Card>
       <Card.Header className="flex-row items-center justify-between">
@@ -10,9 +17,9 @@ export const OrdersByMonthCard = () => {
         <Utensils className="size-5 text-muted-foreground" />
       </Card.Header>
       <Card.Content className="space-y-2">
-        <span className="block text-3xl font-bold">246</span>
+        <span className="block text-3xl font-bold">{data.amount}</span>
         <span className="block text-xs text-muted-foreground">
-          <span className="text-emerald-500 dark:text-emerald-400">+6%</span> em relação ao mês passado
+          <ShowDiff value={data.diffFromLastMonth} /> em relação ao mês passado
         </span>
       </Card.Content>
     </Card>
