7267ff3b37638f4ffe64e8e2ccbaeab10367e73c
Adeonir Kohl <adeonir@gmail.com>
2024-03-12 08:17:31 -0300

    Add memory router to tests custom render

--- src/lib/tests.tsx
+++ src/lib/tests.tsx
@@ -3,15 +3,22 @@ import type { RenderOptions, RenderResult } from '@testing-library/react'
 import { render } from '@testing-library/react'
 import { userEvent } from '@testing-library/user-event'
 import type { ReactElement } from 'react'
+import { MemoryRouter } from 'react-router-dom'
 
 import { Tooltip } from '~/components/ui/tooltip'
 import { Theme } from '~/providers/theme'
 
-const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>): RenderResult =>
+interface CustomRenderOptions extends RenderOptions {
+  initialRoutes?: string[]
+}
+
+const customRender = (ui: ReactElement, options?: CustomRenderOptions): RenderResult =>
   render(ui, {
     wrapper: ({ children }) => (
       <Theme.Provider defaultTheme="light" storageKey="pizza-shop-theme">
-        <Tooltip.Provider>{children}</Tooltip.Provider>
+        <Tooltip.Provider>
+          <MemoryRouter initialEntries={options?.initialRoutes}>{children}</MemoryRouter>
+        </Tooltip.Provider>
       </Theme.Provider>
     ),
     ...options,
