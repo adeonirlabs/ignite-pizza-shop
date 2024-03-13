c7e4195f3b04391f9e54efbf1185adacf4ec9e25
Adeonir Kohl <adeonir@gmail.com>
2024-03-13 09:00:44 -0300

    Add metrics mock to msw setup

--- src/api/mocks/msw.ts
+++ src/api/mocks/msw.ts
@@ -2,10 +2,20 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '~/env'
 
+import { dayOrders, dayRevenue, monthCanceledOrders, monthOrders, monthRevenue, popularProducts } from '../metrics/mock'
 import { signIn } from '../sign-in/mock'
 import { signUp } from '../sign-up/mock'
 
-export const worker = setupWorker(signIn, signUp)
+export const worker = setupWorker(
+  dayOrders,
+  dayRevenue,
+  monthCanceledOrders,
+  monthOrders,
+  monthRevenue,
+  popularProducts,
+  signIn,
+  signUp,
+)
 
 export async function startMsw() {
   if (env.MODE !== 'test') return
