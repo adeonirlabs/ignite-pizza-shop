bcb6ade22b7a805046b488d2e61627e188079fc5
Adeonir Kohl <adeonir@gmail.com>
2024-03-12 23:31:10 -0300

    Add sign-in mock to msw setup

--- src/api/mocks/msw.ts
+++ src/api/mocks/msw.ts
@@ -1,8 +1,9 @@
 import { setupWorker } from 'msw/browser'
 
+import { signIn } from '~/api/sign-in/mock'
 import { env } from '~/env'
 
-export const worker = setupWorker()
+export const worker = setupWorker(signIn)
 
 export async function startMsw() {
   if (env.MODE !== 'test') return
