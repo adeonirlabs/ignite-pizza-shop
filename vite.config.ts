523f9b579a734b8f02dda12dbe4f86ddd20ebedc
Adeonir Kohl <adeonir@gmail.com>
2024-03-15 13:40:37 -0300

    Add manual chunk code spliting to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@
 
 import path from "node:path"
 
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   server: {
     port: 3000,
   },
@@ -21,5 +21,17 @@ export default defineConfig({
     include: ['src/**/*.spec.{ts,tsx}'],
     exclude: ['/node_modules/', '/.next/', 'src/tests/**/*'],
     setupFiles: './vitest.setup.ts',
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.includes("node_modules")) {
+            return id.split("/node_modules/").pop()?.split("/")[0]
+          }
+          return null
+        },
+      },
+    },
   },
 })
